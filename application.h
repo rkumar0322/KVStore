b4fb365db7aafdaa0817f86bedad98033298b3e9
Xinhan-Yan <yan.xi@husky.neu.edu>
2020-04-06 17:11:14 -0400

    Add files via upload

---
+++ application.h
@@ -0,0 +1,95 @@
+#include "object.h"
+#include "kdstore.h"
+#include "keyvalue.h"
+
+class Application: public Object 
+{
+public:
+    KDStore* kv;
+    size_t index;
+
+    Application(size_t idx) {
+        kv = new KDStore();
+        index = idx;
+    };
+
+    ~Application() {
+        delete kv;
+    }
+
+    virtual void run_() {
+
+    }
+
+    size_t this_node() {
+        return index;
+    }
+
+
+};
+
+class Trivial : public Application {
+ public:
+  Trivial(size_t idx) : Application(idx) { }
+  void run_() {
+    size_t SZ = 1000*1000;
+    double* vals = new double[SZ];
+    double sum = 0;
+    for (size_t i = 0; i < SZ; ++i) sum += vals[i] = i;
+    String* s = new String("triv");
+    Key* key = new Key(s,0);
+    DataFrame* df = DataFrame::fromArray(key, kv, SZ, vals);
+    assert(df->get_double(0,1) == 1);
+    DataFrame* df2 = kv->get(key);
+    for (size_t i = 0; i < SZ; ++i) sum -= df2->get_double(0,i);
+    assert(sum==0);
+    delete df; delete df2;
+  }
+};
+
+class Demo : public Application {
+public:
+  Key* main;
+  Key* verify;
+  Key* check;
+ 
+  Demo(size_t idx): Application(idx) {
+      String* s1 = new String("main");
+      main = new Key(s1, 0);
+      String* s2 = new String("verif");
+      verify = new Key(s2, 0);
+      String* s3 = new String("check");
+      check = new Key(s3, 0);
+  }
+ 
+  void run_() override {
+    switch(this_node()) {
+    case 0:   producer();     break;
+    case 1:   counter();      break;
+    case 2:   summarizer();
+   }
+  }
+ 
+  void producer() {
+    size_t SZ = 100*1000;
+    double* vals = new double[SZ];
+    double sum = 0;
+    for (size_t i = 0; i < SZ; ++i) sum += vals[i] = i;
+    DataFrame::fromArray(main, kv, SZ, vals);
+    DataFrame::fromScalar(check, kv, sum);
+  }
+ 
+  void counter() {
+    DataFrame* v = kv->waitandget(main);
+    size_t sum = 0;
+    for (size_t i = 0; i < 100*1000; ++i) sum += v->get_double(0,i);
+    p("The sum is  ").pln(sum);
+    DataFrame::fromScalar(verify, kv, sum);
+  }
+ 
+  void summarizer() {
+    DataFrame* result = kv->waitandget(verify);
+    DataFrame* expected = kv->waitandget(check);
+    pln(expected->get_double(0,0)==result->get_double(0,0) ? "SUCCESS":"FAILURE");
+  }
+};
---
+++ report.md
@@ -0,0 +1,36 @@
+# Introduction 
+eau2 system is a distributed key-value store which stores key-value pairs. Every node in the networks keeps track of its own key value stores, and they are able to get access to other nodes inside the application. 
+
+# Architecture 
+
+
+
+
+
+# Implementation 
+## Dataframe
+- The Dataframe class is the representation of sorer data in a table format. It contains a list of Column to keep track of its data, and a schema which defines the structure of data table. Dataframe unable users to get data with a specifc coordinate and create new dataframe using several different methods. 
+
+## Key-Value store
+- A Key is a String which represents the name key-value pairs under each node.
+- A Value is a char* which represents the char array representation of a Dataframe after being serialized.
+- A Map is a self-defined class which stores a list of Keys and a list of Values with the same size. Every key is corresponding to the value with the same index in their lists. 
+- A Kdstore stores the key-value pairs by having a map as its field. Users are able to access values and insert key-values pairs. 
+
+## Serialize/Deserialize
+- Serial.h has our self-defined classes of Serializer and Deserialzer.
+- Serializer unable users to serialize any primitive types and their corresponding arrays and store their char* representation into its buffer field. 
+- Deserializer unable users to input char* and deserialize it into the actual of its corresponding type.
+- Both serializer and deserializer has an index field which keeps track of the current address in the buffer and updates every time when serializing/deserializing.
+
+## Use Cases
+- To create a DataFrame, assign it as the value of a key, and insert the keyvalue pair into a KDStore:
+    size_t SZ = 1000*1000;
+    double* vals = new double[SZ];
+    double sum = 0;
+    KDStore* kv = new KDStore();
+    for (size_t i = 0; i < SZ; ++i) sum += vals[i] = i;
+    String* s = new String("triv");
+    Key* key = new Key(s, 0);
+    DataFrame* df = DataFrame::fromArray(key, kv, SZ, vals);
+
