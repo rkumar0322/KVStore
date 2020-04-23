50fdc0d612304924c2a1175232f9828cb715bb63
Xinhan-Yan <yan.xi@husky.neu.edu>
2020-04-23 18:38:44 -0400

    Add files via upload

---
+++ test_keyvalue.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <assert.h>
+#include <stdio.h>
+#include "KDStore/keyvalue.h"
+
+void test_key() {
+    String* k1 = new String("hello");
+    Key* key1 = new Key(k1, 0);
+    Serializer ser;
+    key1->serialize(ser);
+    Deserializer dser(ser);
+    Key* key2 = new Key(dser);
+    assert(key1->equals(key2));
+
+    std::cout << "test_key passed" << "\n";
+}
+
+void test_value() {
+    char c1[] = "world";
+    char c2[] = "John";
+    char* cp1 = c1;
+    char* cp2 = c2;
+    Value* v1 = new Value(c1);
+    Serializer ser;
+    v1->serialize(ser);
+    Deserializer dser(ser);
+    Value* v2 = new Value(dser);
+    assert(v1->equals(v2));
+    std::cout << "test_value passed" << "\n";
+
+}
+
+void test_KVPair() {
+    String* k1 = new String("hello");
+    Key* key1 = new Key(k1, 0);
+    char c1[] = "world";
+    char* cp1 = c1;
+    Value* v1 = new Value(c1);
+    KVPair* kv = new KVPair(*key1, *v1);
+    Serializer ser;
+    kv->serialize(ser);
+    Deserializer dser(ser);
+    KVPair* kv2 = new KVPair(dser);
+    assert(kv->equals(kv2));
+    std::cout << "test_value passed" << "\n";
+
+}
+
+int main() {
+    test_key();
+    test_value();
+    test_KVPair();
+    return 0;
+}
---
+++ test_map.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <assert.h>
+#include <stdio.h>
+#include "KDStore/map.h"
+
+void test_map() {
+    Map* m1 = new Map();
+    Map* m2 = new Map();
+    assert(m1->equals(m2));
+    String* k1 = new String("hello");
+    char c1[] = "world";
+    char c2[] = "John";
+    char* cp1 = c1;
+    char* cp2 = c2;
+    Value* v1 = new Value(c1);
+    Value* v2 = new Value(c2);
+
+    assert(!m1->put_(k1, v1));
+    assert(!m1->equals(m2));
+    m2->put_(k1, v1);
+    assert(m1->equals(m2));
+    assert(m1->put_(k1, v2));
+    assert(!m1->equals(m2));
+    std::cout << "test_map passed" << "\n";
+}
+
+void test_map2() {
+    Map* m1 = new Map();
+    Map* m2 = new Map();
+    assert(m1->equals(m2));
+    String* k1 = new String("hello");
+    char c[] = "world";
+    char* c1 = c;
+    Value* v1 = new Value(c1);
+
+    m1->put_(k1, v1);
+    assert(m1->get_(k1)->equals(v1));
+    assert(m2->get_(k1)->equals(nullptr));
+    std::cout << "test_map2 passed" << "\n";
+
+}
+
+int main(int argc, char** argv) {
+    test_map();
+    test_map2();
+    return 0;
+}
+
+
+
+
+
